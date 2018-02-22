9389b9751a406cf23b6370f2d2b93fc6c5001342
David Lance <dcl71@case.edu>
2018-02-22 13:27:15 -0500

    Add POST drop ID request

--- syncr_tracker/tracker.py
+++ syncr_tracker/tracker.py
@@ -1,26 +1,63 @@
-import pickle
+import datetime
 import socket
 import sys
 
+import bencode
+from constants import DROP_ID_BYTE_SIZE
+from constants import DROP_IP_INDEX
+from constants import DROP_NODE_INDEX
+from constants import DROP_PORT_INDEX
+from constants import KEY_INDEX
+from constants import NODE_ID_BYTE_SIZE
+from constants import TYPE_INDEX
+from constants import VALUE_INDEX
 
-def handle_request(data, addr):
-    request = pickle.loads(data)
+drop_availability = dict()
 
-    if request[0] == 'GET':
-        handle_get(data, addr)
-    elif request[0] == 'POST':
-        handle_post(data, addr)
+
+def handle_request(conn, request, addr):
+    if request[TYPE_INDEX] == 'GET':
+        print("GET request")
+        handle_get(conn, request, addr)
+    elif request[TYPE_INDEX] == 'POST':
+        print("POST request")
+        handle_post(conn, request, addr)
     else:
         # Return some sort of error
         pass
 
 
-def handle_get(data, addr):
+def handle_get(conn, request, addr):
     pass
 
 
-def handle_post(data, addr):
+def handle_post(conn, request, addr):
+    if len(request[KEY_INDEX]) == NODE_ID_BYTE_SIZE:
+        request_post_node_id(conn, request, addr)
+    if len(request[KEY_INDEX]) == DROP_ID_BYTE_SIZE:
+        request_post_drop_id(conn, request, addr)
+
+
+def request_post_node_id(conn, request, addr):
     pass
+
+
+def request_post_drop_id(conn, request, addr):
+    if request[KEY_INDEX] in drop_availability.keys():
+        drop_availability[request[KEY_INDEX]].append(
+            request[VALUE_INDEX].append(datetime.datetime),
+        )
+    else:
+        drop_availability[request[KEY_INDEX]] = [
+            request[VALUE_INDEX].append(datetime.datetime),
+        ]
+    print(
+        "Drop Availability Updated - ", request[KEY_INDEX],
+        "\n\tNode: ", request[VALUE_INDEX][DROP_NODE_INDEX],
+        "\n\tIP: ", request[VALUE_INDEX][DROP_IP_INDEX],
+        "\n\tPort: ", request[VALUE_INDEX][DROP_PORT_INDEX],
+    )
+    conn.send(bencode.encode('Drop availability updated'))
 
 
 def main():
@@ -28,24 +65,24 @@ def main():
 
     :return:
     """
-    TCP_IP = sys.argv[1]
-    TCP_PORT = sys.argv[2]
-    BUFFER_SIZE = 4096
+    tcp_ip = sys.argv[1]
+    tcp_port = sys.argv[2]
+    buffer_size = 4096
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((TCP_IP, int(TCP_PORT)))
+    s.bind((tcp_ip, int(tcp_port)))
     s.listen(1)
 
     while 1:
         conn, addr = s.accept()
         print('Connection address:', addr)
         while 1:
-            data = conn.recv(BUFFER_SIZE)
+            data = conn.recv(buffer_size)
             if not data:
                 break
-            handle_request(data, addr)
-            print("Received data.")
-            conn.send(data)  # echo
+            print('Data received')
+            request = bencode.decode(data)
+            handle_request(conn, request, addr)
         conn.close()
 
 
